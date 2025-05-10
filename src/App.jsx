7bf681c8a9c411dfafa0ff14881c4d24d34c7397
Harshada Talekar <harshadatalekar27@gmail.com>
2025-05-10 16:10:03 +0530

    Initial commit with .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
+
+const App = () => {
+
+  const [sidebar,setSidebar] = useState(true)
+
+  return (
+    <div>
+      <Navbar setSidebar={setSidebar} />
+      <Routes>
+        <Route path='/' element={<Home sidebar={sidebar} />} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
