09396787e17d1cdecb00805827cb2fec80de2ee6
Harshada Talekar <149597781+talekarharshu@users.noreply.github.com>
2025-06-02 12:51:55 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,8 @@ import search_icon from "../../assets/search.png";
 import upload_icon from "../../assets/upload.png";
 import more_icon from "../../assets/more.png";
 import notification_icon from "../../assets/notification.png";
-import profile_icon from "../../assets/jack.png";
+// import profile_icon from "../../assets/jack.png";
+import profile_icon from "../../assets/jack.jpg";
 import { Link, useNavigate } from "react-router-dom";
 
 
